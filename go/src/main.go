31b5b7545397f05c74849dfb2f96e921ba9f9210
Garrison Pennington <garrisonpennington99@gmail.com>
2022-04-28 14:43:01 +0200

    Make main a cli application

--- go/src/main.go
+++ go/src/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"flag"
 	cmp "fractals/forms/complex"
 	mand "fractals/forms/complex/examples"
+	"os"
 
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 var MANDELBROT mand.Mandelbrot = mand.Mandelbrot{}
@@ -43,6 +46,70 @@ var REACHED_STEPS_DEFAULT func(cmp.ComplexFractalValue) bool = cmp.BinStepScorer
 
 func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	UHD_MANDELBROT_FULL.MultiPhaseRender(5, WINDOW_10X10, 5).Save()
+	resPtr := flag.String("resolution", "UHD", "Render output resolution")
+	fractalPtr := flag.String("fractal", "mandelbrot", "The fractal to render")
+	stepsPtr := flag.Uint("steps", 100, "Number of iterations to run on each point")
+	phasesPtr := flag.Uint("phases", 0, "Number of zoom phases to perform in MultiPhaseRender, 0 is regular render")
+	windowPtr := flag.Int("window-size", 10, "Side length of the MultiPhaseWindow")
+	stridePtr := flag.Int("stride", 5, "Stride of the MultiPhaseWindow")
+	args := os.Args[1:]
+	switch action := args[0]; action {
+	case "render":
+		render := cmp.ComplexRender{
+			Resolution: parseResolution(*resPtr),
+			Bounds:     parseBounds(*fractalPtr),
+			Fractal:    parseFractal(*fractalPtr),
+			Steps:      uint32(*stepsPtr),
+		}
+		if *phasesPtr > 0 {
+			window := cmp.Window{
+				Height: *windowPtr,
+				Width:  *windowPtr,
+				Array:  nil,
+			}
+			render.MultiPhaseRender(uint8(*phasesPtr), window, *stridePtr).Save()
+		} else {
+			render.Save()
+		}
+		break
+	case "music":
+		break
+	}
 
+	//UHD_MANDELBROT_FULL.MultiPhaseRender(5, WINDOW_10X10, 5).Save()
+
+}
+
+func parseResolution(resStr string) cmp.Resolution {
+	switch resStr {
+	case "UHD":
+		return UHD
+	}
+	log.Fatal().Msgf("Invalid render resolution given: %v", resStr)
+	return UHD // TODO: Make nil value
+}
+
+func parseBounds(fractalStr string) cmp.Bounds {
+	switch fractalStr {
+	case "mandelbrot":
+		return MANDELBROT_WHOLE_BOUNDS
+	case "divmod":
+		return cmp.Bounds{
+			Lower: cmp.MakeComplexPoint(complex(-5, -5)),
+			Upper: cmp.MakeComplexPoint(complex(5, 5)),
+		}
+	}
+	log.Fatal().Msgf("Invalid render bounds given: %v", fractalStr)
+	return MANDELBROT_WHOLE_BOUNDS // TODO: Make nil value
+}
+
+func parseFractal(fractalStr string) cmp.ComplexFractal {
+	switch fractalStr {
+	case "mandelbrot":
+		return MANDELBROT
+	case "divmod":
+		break // TODO: Merge new-fractal branch and add DivMod here
+	}
+	log.Fatal().Msgf("Invalid render resolution given: %v", fractalStr)
+	return MANDELBROT // TODO: Make nil value
 }
