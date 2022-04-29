c39713a55161f538cb2b528ba99f69649547ab2c
Garrison Pennington <garrisonpennington99@gmail.com>
2022-04-29 16:50:10 +0200

    add map from note values to note fns

--- go/src/forms/music/pink.go
+++ go/src/forms/music/pink.go
@@ -1,1 +1,11 @@
 package music
+
+import smf "gitlab.com/gomidi/midi/v2/smf"
+
+var NOTE_VALUES map[uint16]func(*smf.Track, smf.MetricTicks, uint8, uint8, uint8) = map[uint16]func(*smf.Track, smf.MetricTicks, uint8, uint8, uint8){
+	1:  Play16th,
+	2:  Play8th,
+	4:  Play4th,
+	8:  PlayHalf,
+	16: PlayWhole,
+}
