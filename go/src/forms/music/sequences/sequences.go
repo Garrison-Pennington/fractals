faa47cc300922bf883d12b0e5a11d9bcc00800f2
Garrison Pennington <garrisonpennington99@gmail.com>
2023-01-02 19:16:37 +0100

    make music_sequences a separate package

--- go/src/forms/music/sequences/sequences.go
+++ go/src/forms/music/sequences/sequences.go
@@ -1,8 +1,19 @@
-package music
+package music_sequences
 
 import (
 	mt "fractals/forms/music/theory"
 )
+
+type NoteSequence interface {
+	Next() mt.Note
+}
+
+func NextNotes(seq NoteSequence, n int) (notes []mt.Note) {
+	for i := 0; i < n; i++ {
+		notes = append(notes, seq.Next())
+	}
+	return
+}
 
 type ToneSequence interface {
 	Next() mt.Tone
